c3f578f12a3f76f22e0f50b2890582bb0e5bc2db
Alexander Leitner <leitnersalex@gmail.com>
2018-04-13 11:25:25 +0900

    Test layout

--- pkg/api_test.go
+++ pkg/api_test.go
@@ -6,11 +6,22 @@ func TestGetStoreInfo(t *testing.T) {
     GetStoreInfo("/tmp/")
 }
 
-func TestDelete(t *testing.T) {
-}
-
 func TestStore(t *testing.T) {
+  // Run Store()
+  // Check if folder structure and file were created
+  // delete the folders and file
 }
 
 func TestRetrieve(t *testing.T) {
+  // Run Store()
+  // Run Retrieve()
+  // Verify that the contents of the retrieve match what was stored
+  // delete the folders and file
+}
+
+func TestDelete(t *testing.T) {
+  // Run Store()
+  // Verify files exist
+  // Run Delete()
+  // Verify that the files are no longer existing
 }
