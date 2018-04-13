45273573cc61c116f918455513a4d96926d00ea2
Alexander Leitner <leitnersalex@gmail.com>
2018-04-13 11:12:06 +0900

    Added test file

---
+++ pkg/api_test.go
@@ -0,0 +1,16 @@
+package piecestore // import "github.com/aleitner/piece-store/pkg"
+
+import "testing"
+
+func TestGetStoreInfo(t *testing.T) {
+    GetStoreInfo("/tmp/")
+}
+
+func TestDelete(t *testing.T) {
+}
+
+func TestStore(t *testing.T) {
+}
+
+func TestRetrieve(t *testing.T) {
+}
