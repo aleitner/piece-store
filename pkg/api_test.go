71e185c5774f57613c6753c3c229c6ba1820f96c
Cameron Ayer <camayer92@gmail.com>
2018-04-18 16:29:36 +0900

    added more test cases

--- pkg/api_test.go
+++ pkg/api_test.go
@@ -15,48 +15,157 @@ import (
 var tmpfile string
 
 func TestStore(t *testing.T) {
-	file, err := os.Open(tmpfile)
-	if err != nil {
-		t.Errorf("Error opening tmp file: %s", err.Error())
-		return
-	}
+	t.Run("it stores data successfully", func(t *testing.T) {
+		file, err := os.Open(tmpfile)
+		if err != nil {
+			t.Errorf("Error opening tmp file: %s", err.Error())
+			return
+		}
 
-	fi, err := file.Stat()
-	if err != nil {
-		t.Errorf("Could not stat test file: %s", err.Error())
-		return
-	}
+		fi, err := file.Stat()
+		if err != nil {
+			t.Errorf("Could not stat test file: %s", err.Error())
+			return
+		}
 
-	defer file.Close()
+		defer file.Close()
 
-	hash := "0123456789ABCDEFGHIJ"
-	Store(hash, file, int64(fi.Size()), 0, os.TempDir())
+		hash := "0123456789ABCDEFGHIJ"
+		Store(hash, file, int64(fi.Size()), 0, os.TempDir())
 
-	folder1 := string(hash[0:2])
-	folder2 := string(hash[2:4])
-	fileName := string(hash[4:])
+		folder1 := string(hash[0:2])
+		folder2 := string(hash[2:4])
+		fileName := string(hash[4:])
 
-	createdFilePath := path.Join(os.TempDir(), folder1, folder2, fileName)
-	defer os.RemoveAll(path.Join(os.TempDir(), folder1))
-	_, lStatErr := os.Lstat(createdFilePath)
-	if lStatErr != nil {
-		t.Errorf("No file was created from Store(): %s", lStatErr.Error())
-		return
-	}
+		createdFilePath := path.Join(os.TempDir(), folder1, folder2, fileName)
+		defer os.RemoveAll(path.Join(os.TempDir(), folder1))
+		_, lStatErr := os.Lstat(createdFilePath)
+		if lStatErr != nil {
+			t.Errorf("No file was created from Store(): %s", lStatErr.Error())
+			return
+		}
 
-	createdFile, openCreatedError := os.Open(createdFilePath)
-	if openCreatedError != nil {
-		t.Errorf("Error: %s opening created file %s", openCreatedError.Error(), createdFilePath)
-	}
-	defer createdFile.Close()
+		createdFile, openCreatedError := os.Open(createdFilePath)
+		if openCreatedError != nil {
+			t.Errorf("Error: %s opening created file %s", openCreatedError.Error(), createdFilePath)
+		}
+		defer createdFile.Close()
 
-	buffer := make([]byte, 5)
-	createdFile.Seek(0, 0)
-	_, _ = createdFile.Read(buffer)
+		buffer := make([]byte, 5)
+		createdFile.Seek(0, 0)
+		_, _ = createdFile.Read(buffer)
 
-	if string(buffer) != "butts" {
-		t.Errorf("Expected data butts does not equal Actual data %s", string(buffer))
-	}
+		if string(buffer) != "butts" {
+			t.Errorf("Expected data butts does not equal Actual data %s", string(buffer))
+		}
+	})
+
+	t.Run("it stores data by offset successfully", func(t *testing.T) {
+		file, err := os.Open(tmpfile)
+		if err != nil {
+			t.Errorf("Error opening tmp file: %s", err.Error())
+			return
+		}
+
+		fi, err := file.Stat()
+		if err != nil {
+			t.Errorf("Could not stat test file: %s", err.Error())
+			return
+		}
+
+		defer file.Close()
+
+		hash := "0123456789ABCDEFGHIJ"
+		Store(hash, file, int64(fi.Size()), 2, os.TempDir())
+
+		folder1 := string(hash[0:2])
+		folder2 := string(hash[2:4])
+		fileName := string(hash[4:])
+
+		createdFilePath := path.Join(os.TempDir(), folder1, folder2, fileName)
+		defer os.RemoveAll(path.Join(os.TempDir(), folder1))
+		_, lStatErr := os.Lstat(createdFilePath)
+		if lStatErr != nil {
+			t.Errorf("No file was created from Store(): %s", lStatErr.Error())
+			return
+		}
+
+		createdFile, openCreatedError := os.Open(createdFilePath)
+		if openCreatedError != nil {
+			t.Errorf("Error: %s opening created file %s", openCreatedError.Error(), createdFilePath)
+		}
+		defer createdFile.Close()
+
+		buffer := make([]byte, 7)
+		createdFile.Seek(0, 0)
+		_, _ = createdFile.Read(buffer)
+
+		// \0\0butts
+		expected := []byte{0, 0, 98, 117, 116, 116, 115}
+
+		if string(buffer) != string(expected) {
+			t.Errorf("Expected data %s does not equal Actual data %s", string(expected), string(buffer))
+		}
+	})
+
+	t.Run("it stores data by chunk successfully", func(t *testing.T) {
+		file, err := os.Open(tmpfile)
+		if err != nil {
+			t.Errorf("Error opening tmp file: %s", err.Error())
+			return
+		}
+
+		defer file.Close()
+
+		hash := "0123456789ABCDEFGHIJ"
+		Store(hash, file, 4, 0, os.TempDir())
+
+		folder1 := string(hash[0:2])
+		folder2 := string(hash[2:4])
+		fileName := string(hash[4:])
+
+		createdFilePath := path.Join(os.TempDir(), folder1, folder2, fileName)
+		defer os.RemoveAll(path.Join(os.TempDir(), folder1))
+		_, lStatErr := os.Lstat(createdFilePath)
+		if lStatErr != nil {
+			t.Errorf("No file was created from Store(): %s", lStatErr.Error())
+			return
+		}
+
+		createdFile, openCreatedError := os.Open(createdFilePath)
+		if openCreatedError != nil {
+			t.Errorf("Error: %s opening created file %s", openCreatedError.Error(), createdFilePath)
+		}
+		defer createdFile.Close()
+
+		buffer := make([]byte, 4)
+		createdFile.Seek(0, 0)
+		_, _ = createdFile.Read(buffer)
+
+		// butt
+		expected := []byte{98, 117, 116, 116}
+
+		if string(buffer) != string(expected) {
+			t.Errorf("Expected data %s does not equal Actual data %s", string(expected), string(buffer))
+		}
+	})
+
+	t.Run("it should return hash err if the hash is too short", func(t *testing.T) {
+		file, err := os.Open(tmpfile)
+		if err != nil {
+			t.Errorf("Error opening tmp file: %s", err.Error())
+			return
+		}
+
+		defer file.Close()
+
+		hash := "01"
+
+		err = Store(hash, file, 5, 0, os.TempDir())
+		if err == nil || err.Error() != "argError: Hash is too short. Must be atleast 20 bytes" {
+			t.Errorf("Expected error (Hash is too short. Must be atleast 20 bytes) does not equal Actual error (%s)", err.Error())
+		}
+	})
 }
 
 func TestRetrieve(t *testing.T) {
