d3675dd770297793f790ac7842767b4fe7cc0f31
Alexander Leitner <leitnersalex@gmail.com>
2018-04-19 15:51:42 +0900

    Render html files

--- server/main.go
+++ server/main.go
@@ -2,71 +2,72 @@ package main
 import (
   "github.com/aleitner/piece-store/pkg"
   "fmt"
+  "github.com/julienschmidt/httprouter"
   "net/http"
-  "strings"
+  "log"
+  "html/template"
 )
 
 const dataDir = "./piece-store-data"
 
-const AddForm = `
-<form enctype="multipart/form-data" action="http://127.0.0.1:8080/upload" method="post">
-{{/* 1. File input */}}
-<input type="file" name="uploadfile" />
+func ReceiveFile(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
-{{/* 2. Submit button */}}
-<input type="submit" value="upload file" />
-</form>
-`
-
-func ReceiveFile(w http.ResponseWriter, r *http.Request) {
-  if r.Method == "GET" {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    fmt.Fprint(w, AddForm)
-
-  } else if r.Method == "POST" {
-    // in your case file would be fileupload
-    file, header, err := r.FormFile("uploadfile")
-    if err != nil {
-      fmt.Printf("Error: ", err.Error())
-      return
-    }
-
-    defer file.Close()
-    fmt.Printf("Uploading file %s...", header.Filename)
-
-    hash := String(20)
-    err = pstore.Store(hash, file, header.Size, 0, dataDir)
-
-    if err != nil {
-      fmt.Printf("Error: ", err.Error())
-    }
-
-    message := fmt.Sprintf("Successfully uploaded file!\nName: %s\nHash: %s\nSize: %v\n", header.Filename, hash, header.Size)
-    w.Write([]byte(message))
-
-
-  } else {
-      fmt.Println("Unknown HTTP " + r.Method + "  Method")
+  // in your case file would be fileupload
+  file, header, err := r.FormFile("uploadfile")
+  if err != nil {
+    fmt.Printf("Error: ", err.Error())
+    return
   }
-}
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-  message := r.URL.Path
-  message = strings.TrimPrefix(message, "/")
-  message = "Hello " + message
+  defer file.Close()
+  fmt.Printf("Uploading file %s...", header.Filename)
+
+  hash := String(20)
+  err = pstore.Store(hash, file, header.Size, 0, dataDir)
+
+  if err != nil {
+    fmt.Printf("Error: ", err.Error())
+    return
+  }
+
+  message := fmt.Sprintf("Successfully uploaded file!\nName: %s\nHash: %s\nSize: %v\n", header.Filename, hash, header.Size)
   w.Write([]byte(message))
 }
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+  if err := renderByPath(w, "./server/templates/index.html"); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+  }
+}
+
+func ShowUploadForm(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+  if err := renderByPath(w, "./server/templates/uploadform.html"); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+  }
+}
+
+func renderByPath(w http.ResponseWriter, path string) error {
+  w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+  fp := http.Dir(path)
+  fmt.Println(fp)
+  tmpl, err := template.ParseFiles(path)
+  if err != nil {
+      return err
+  }
+
+  tmpl.Execute(w, "")
+  return nil
+}
 
 func main() {
   fmt.Println("Starting server...")
-  http.HandleFunc("/", sayHello)
-  http.HandleFunc("/upload", ReceiveFile)
-
-  fs := http.FileServer(http.Dir(dataDir))
-  http.Handle("/files/", http.StripPrefix("/files", fs))
-
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
+  router := httprouter.New()
+  router.GET("/", Index)
+  router.GET("/upload", ShowUploadForm)
+  router.ServeFiles("/files/*filepath", http.Dir(dataDir))
+  router.POST("/upload", ReceiveFile)
+  log.Fatal(http.ListenAndServe(":8080", router))
 }
