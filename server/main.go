ca430f4420822a8111e2a80bf8a242894a67e364
Cameron Ayer <camayer92@gmail.com>
2018-04-20 13:38:01 +0900

    set up download endpoint

--- server/main.go
+++ server/main.go
@@ -49,7 +49,7 @@ func UploadFile(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
   err = pstore.Store(hash, file, dataSize, dataOffset, dataDir)
 
   if err != nil {
-    fmt.Printf("Error: ", err.Error())
+    fmt.Printf("Error: %s\n", err.Error())
     return
   }
 
@@ -62,6 +62,50 @@ func UploadFile(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
   w.Write([]byte(message))
 }
 
+func DownloadFile(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+  r.ParseForm()
+
+  hash := strings.Join(r.Form["hash"], "")
+
+  var length int64
+  lengthStr:= strings.Join(r.Form["length"], "")
+  length64, _ := strconv.ParseInt(lengthStr, 10, 64)
+  if lengthStr == "" && length64 <=0 {
+    length = 0
+  } else {
+    length = length64
+  }
+
+  var dataOffset int64
+  dataOffsetStr := strings.Join(r.Form["offset"], "")
+  dataOffsetInt64, _ := strconv.ParseInt(dataOffsetStr, 10, 64);
+  if dataOffsetStr == "" && dataOffsetInt64 <= 0 {
+    dataOffset = 0
+  } else {
+    dataOffset = dataOffsetInt64
+  }
+  fmt.Printf("Downloading file (%s), Offset: (%v), Size: (%v)...\n", hash, dataOffset, length)
+
+  err := pstore.Retrieve(hash, w, length, dataOffset, dataDir)
+
+  if err != nil {
+    fmt.Printf("Error: %s\n", err.Error())
+    w.Write([]byte(err.Error()))
+    return
+  }
+
+  w.Header().Set("Content-Disposition", "attachment; filename="+hash)
+  w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+
+  fmt.Printf("Successfully downloaded file %s...\n", hash)
+
+
+  // w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  // message := fmt.Sprintf("Successfully downloaded file! \nHash: %s\nSize: %v\n", hash, length)
+  // message = fmt.Sprintf("%s\n<a href=\"/files/\">List files</a>", message)
+  // w.Write([]byte(message))
+}
+
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
   if err := renderByPath(w, "./server/templates/index.html"); err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -71,6 +115,13 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 func ShowUploadForm(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
   if err := renderByPath(w, "./server/templates/uploadform.html"); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+  }
+}
+
+func ShowDownloadForm(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+  if err := renderByPath(w, "./server/templates/downloadform.html"); err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
   }
@@ -92,7 +143,9 @@ func main() {
   router := httprouter.New()
   router.GET("/", Index)
   router.GET("/upload", ShowUploadForm)
+  router.GET("/download", ShowDownloadForm)
   router.ServeFiles("/files/*filepath", http.Dir(dataDir))
   router.POST("/upload", UploadFile)
+  router.POST("/download", DownloadFile)
   log.Fatal(http.ListenAndServe(":8080", router))
 }
