7898e075e8df4d5c09d3b14583f45c4b2be4638c
Alexander Leitner <leitnersalex@gmail.com>
2018-05-03 16:49:28 +0900

    Cleaned server code

---
+++ rpc-server/api/api.go
@@ -0,0 +1,35 @@
+// Package main implements a simple gRPC server that demonstrates how to use gRPC-Go libraries
+// to perform unary, client streaming, server streaming and full duplex RPCs.
+//
+// It implements the route guide service whose definition can be found in routeguide/route_guide.proto.
+package api
+
+import (
+	"fmt"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/aleitner/piece-store/routeguide"
+)
+
+type Server struct {
+  // Put resources here that you want available inside RPC calls
+}
+
+func (s *Server) Store(stream pb.RouteGuide_StoreServer) error {
+  fmt.Println("Storing data")
+
+  return nil
+}
+
+func (s *Server) Retrieve(rect *pb.ShardRetrieval, stream pb.RouteGuide_RetrieveServer) error {
+  fmt.Println("Retrieving data")
+
+  return nil
+}
+
+func (s *Server) Delete(context.Context, *pb.ShardDelete) (*pb.Summary, error) {
+  fmt.Println("Deleting data")
+
+  return nil, nil
+}
---
+++ rpc-server/main.go
@@ -0,0 +1,36 @@
+// Package main implements a simple gRPC server that demonstrates how to use gRPC-Go libraries
+// to perform unary, client streaming, server streaming and full duplex RPCs.
+//
+// It implements the route guide service whose definition can be found in routeguide/route_guide.proto.
+package main
+
+import (
+	"fmt"
+  "log"
+	"net"
+
+	"google.golang.org/grpc"
+
+  "github.com/aleitner/piece-store/rpc-server/api"
+  pb "github.com/aleitner/piece-store/routeguide"
+
+)
+
+func main() {
+  // create a listener on TCP port 7777
+  lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 7777))
+  if err != nil {
+    log.Fatalf("failed to listen: %v", err)
+  }
+  // create a server instance
+  s := api.Server{}
+
+  // create a gRPC server object
+  grpcServer := grpc.NewServer()
+  // attach the Ping service to the server
+  pb.RegisterRouteGuideServer(grpcServer, &s)
+  // start the server
+  if err := grpcServer.Serve(lis); err != nil {
+    log.Fatalf("failed to serve: %s", err)
+  }
+}
